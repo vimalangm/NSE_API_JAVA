a6e047508fbc139bc21901361dbea9e3b169d5b3
G <vimlnu@deloitte.com>
2023-08-06 19:30:18 +0530

    spring boot nse

---
+++ nse/src/main/java/com/nse/nse/NseApplication.java
@@ -0,0 +1,13 @@
+package com.nse.nse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NseApplication.class, args);
+	}
+
+}
